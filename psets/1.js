576673515b0e441f409b3efdda60759f93387dc1
Yohana Casas <Yohana Casas>
2016-05-19 21:38:45 -0400

    working on pset 1

--- psets/1.js
+++ psets/1.js
@@ -8,5 +8,3 @@ Forgot how old someone is? Calculate it!
 - Calculate their 2 possible ages based on the stored values.
 - Output them to the screen like so: "They are either NN or NN", substituting the values.
 */
-
-// write your solution here...
